a719a2272d08b2f6a5dbf31888a5db2b7bc1769b
Ellen153 <141336693+Ellen153@users.noreply.github.com>
2024-02-22 08:58:27 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`conteudo inicial da variavel: ${pessoa} | tipo: ${typeof(pessoa) }`);
+
+pessoa = 259;
+
+console.log(`mudei o conteudo: ${pessoa} |tipo: ${typeof(pessoa)}`);
